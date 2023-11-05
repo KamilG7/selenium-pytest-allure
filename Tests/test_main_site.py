8b5948f5dbb98aee97c5a8be5916a5ff27cda947
kamil <kamilgaldy@gmail.com>
2023-11-05 14:26:32 +0100

    Second page object

--- Tests/test_main_site.py
+++ Tests/test_main_site.py
@@ -7,8 +7,8 @@ from pages.main_site_page import MainSitePage
 @pytest.mark.usefixtures("setup")
 class TestMainSite:
 
-    @allure.title("Id 1010")
-    @allure.description("Registration should fail with empty form")
+    @allure.title("Id 1001")
+    @allure.description("Checking if main page loads")
     def check_if_page_loads(self, setup):
         self.driver.get("https://ultimateqa.com/")
         main_site_page = MainSitePage(self.driver)
