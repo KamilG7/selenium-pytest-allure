c97fa35d260530cf569e85704a08b6390c88028c
kamil <kamilgaldy@gmail.com>
2023-11-05 13:33:30 +0100

    Adding test methods to registration tests script

--- Tests/test_registration.py
+++ Tests/test_registration.py
@@ -11,31 +11,23 @@ password = "StrongPassword123"
 incorrect_email = "xxx.com"
 weak_password = "xxx"
 
+
 @pytest.mark.usefixtures("setup")
 class TestRegistration:
 
-
     @allure.title("Id 1010")
     @allure.description("Registration should fail with empty form")
-    def test_registration_empty_form(self,setup):
-
+    def test_registration_empty_form(self, setup):
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.click_submit_registration()
         error_list = registration_page.capture_error_message()
         assert "First name can't be blank" in error_list, "/First name can't be blank/ has to be displayed"
-        assert "Last name can't be blank" in error_list, "/Last name can't be blank/ has to be displayed"
-        assert "Terms must be accepted" in error_list, "/Terms must be accepted/ has to be displayed"
-        assert "Email can't be blank" in error_list, "/Email can't be blank/ has to be displayed"
-        assert "Password can't be blank" in error_list, "/Password can't be blank/ has to be displayed"
         allure.attach(self.driver.get_screenshot_as_png(), name="All errors", attachment_type=AttachmentType.PNG)
-
-
 
     @allure.title("Id 1005")
     @allure.description("Registration should fail when 'First Name' not submitted")
     def test_registration_no_name(self, setup):
-
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.last_name_input_registration(last_name)
@@ -47,11 +39,9 @@ class TestRegistration:
         allure.attach(self.driver.get_screenshot_as_png(), name="No name error", attachment_type=AttachmentType.PNG)
         assert "First name can't be blank" in error_list, "/First name can't be blank/ has to be visible!"
 
-
     @allure.title("Id 1006")
     @allure.description("Registration should fail when 'Last Name' not submitted")
     def test_registration_no_last_name(self, setup):
-
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.name_input_registration(first_name)
@@ -67,7 +57,6 @@ class TestRegistration:
     @allure.title("Id 1007")
     @allure.description("Registration should fail when 'Email' not submitted")
     def test_registration_no_email(self, setup):
-
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.name_input_registration(first_name)
@@ -82,7 +71,7 @@ class TestRegistration:
 
     @allure.title("Id 1008")
     @allure.description("Registration should fail when incorrect 'Email' submitted")
-    def test_registration_no_email(self, setup):
+    def test_registration_incorrect_email(self, setup):
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.name_input_registration(first_name)
@@ -98,7 +87,7 @@ class TestRegistration:
 
     @allure.title("Id 1009")
     @allure.description("Registration should fail when weak password submitted")
-    def test_registration_no_email(self, setup):
+    def test_registration_weak_password(self, setup):
         self.driver.get("https://courses.ultimateqa.com/users/sign_up")
         registration_page = RegistrationPage(self.driver)
         registration_page.name_input_registration(first_name)
@@ -113,6 +102,63 @@ class TestRegistration:
         assert "Password must be at least 8 characters" in error_list, "/Password must be at least 8 characters/" \
                                                                        " has to be visible!"
 
+    @allure.title("Id 1011")
+    @allure.description("User should be able to open Privacy Policy")
+    def test_registration_privacy_policy(self, setup):
+        self.driver.get("https://courses.ultimateqa.com/users/sign_up")
+        registration_page = RegistrationPage(self.driver)
+        registration_page.open_privacy_policy()
+        privacy_handle = self.driver.window_handles[1]
+        self.driver.switch_to.window(privacy_handle)
+        title_privacy = self.driver.title
+        assert title_privacy == "Privacy Policy", f"Privacy terms should be displayed, is {title_privacy}"
+        allure.attach(self.driver.get_screenshot_as_png(), name="privacy policy", attachment_type=AttachmentType.PNG)
 
+    @allure.title("Id 1012")
+    @allure.description("User should be able to open Terms of use")
+    def test_registration_terms_of_use(self, setup):
+        self.driver.get("https://courses.ultimateqa.com/users/sign_up")
+        registration_page = RegistrationPage(self.driver)
+        registration_page.open_terms_of_use()
+        try:
+            terms_handle = self.driver.window_handles[2]
+        except:
+            print("Not all test cases run, reassessment of window handle number")
+            terms_handle = self.driver.window_handles[1]
+        self.driver.switch_to.window(terms_handle)
+        title_terms = self.driver.title
+        assert title_terms == "Terms of Use", f"terms should be displayed, is {title_terms}"
+        allure.attach(self.driver.get_screenshot_as_png(), name="Terms of use", attachment_type=AttachmentType.PNG)
+
+    @allure.title("Id 1003")
+    @allure.description("Registration should succeed with valid input")
+    def test_correct_registration(self, setup):
+        self.driver.get("https://courses.ultimateqa.com/users/sign_up")
+        registration_page = RegistrationPage(self.driver)
+        registration_page.name_input_registration(first_name)
+        registration_page.last_name_input_registration(last_name)
+        registration_page.email_input_registration(email)
+        registration_page.password_input_registration(password)
+        registration_page.terms_checkbox_click()
+        registration_page.click_submit_registration()
+        allure.attach(self.driver.get_screenshot_as_png(), name="Correct registration",
+                      attachment_type=AttachmentType.PNG)
+        registration_page.log_out()
+
+    @allure.title("Id 1013")
+    @allure.description("Registration should failed with used email")
+    def test_registration_used_email(self, setup):
+        self.driver.get("https://courses.ultimateqa.com/users/sign_up")
+        registration_page = RegistrationPage(self.driver)
+        registration_page.name_input_registration(first_name)
+        registration_page.last_name_input_registration(last_name)
+        registration_page.email_input_registration(email)
+        registration_page.password_input_registration(password)
+        registration_page.terms_checkbox_click()
+        registration_page.click_submit_registration()
+        error_email_registered = registration_page.capture_error_message()
+        assert "Email has already been taken" in error_email_registered, "There can't be two users with same email"
+        allure.attach(self.driver.get_screenshot_as_png(), name="Correct registration",
+                      attachment_type=AttachmentType.PNG)
 
 
